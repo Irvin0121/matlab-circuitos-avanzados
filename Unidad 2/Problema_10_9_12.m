0558f36a9616f75dda9de83dc1541d19775fd412
Irvin Ramírez <54339668+Irvin0121@users.noreply.github.com>
2020-12-13 00:01:21 -0600

    Agregar correcta impresión

--- Unidad 2/Problema_10_9_12.m
+++ Unidad 2/Problema_10_9_12.m
@@ -1,15 +1,11 @@
 %Problema 10.9-12
+%Irvin Adolfo Ramírez Torres 1MM3
 
 clc;
 
-s1="Va: ";
-s2="Vb: ";
 A=[-17/120 1/20; -1/20 11/100];
 B=[-3/5; 24/25];
-
 X=linsolve(A,B);
 
-disp(s1)
-disp(X(1))
-disp(s2)
-disp(X(2))
+disp(['Va= ', num2str(X(1)), ' V'])
+disp(['Vb= ', num2str(X(2)), ' V'])
