abd52a3a67c2c044784496bca939d80f50f1daf3
Irvin Ramírez <54339668+Irvin0121@users.noreply.github.com>
2020-12-12 23:55:59 -0600

    Agregar cambios y mostrar correctamente

--- Unidad 2/Problema_10_9_18.m
+++ Unidad 2/Problema_10_9_18.m
@@ -1,10 +1,15 @@
 %Problema 10.9-18
+%Irvin Adolfo Ramírez Torres 1MM3
 clc;
-%Malla 1:24∠15°=I_1 (25+j40)-25I_2-(j40)I_3
+
+%Malla 1: I_1 (25+j40)-25I_2-(j40)I_3=24∠15°
 %Malla 2: -25I_1+(25+10-j6.25) I_2-10I_3=0
 %Malla 3: -(j40)I_1-10I_2+(45+10+j40)I_3=0
 
 A=[25+40i -25 -40i; -25 35-6.25i -10; -40i -10 55+40i];
-B=[23.18221983+6.211657082i; 0; 0];
-X=linsolve(A,B);
-disp(X)
+B=[24*expm(1i*deg2rad(15)); 0; 0];
+I=linsolve(A,B);
+it=(I(3)-I(2));
+[Th, r]=cart2pol(real(it), imag(it));
+Th=rad2deg(Th);
+disp(['i(t)= ', num2str(r), 'cos(10t+', num2str(Th), ')'])
